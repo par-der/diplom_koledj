a37d334448409a89a2d1216c035832dedc8f49df
6ivKin <fedorterentev48@gmail.com>
2023-04-01 22:01:54 +0400

    Соединил бэк с фронтом.

---
+++ app/Http/Controllers/API/Product/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API\Product;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Product\ProductResource;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\ProductTag;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function __invoke()
+    {
+        $products = Product::all();
+        return ProductResource::collection($products);
+    }
+}
