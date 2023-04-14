0451681c8ac0865ca48cad8181a5b50c3089e53f
6ivKin <fedorterentev48@gmail.com>
2023-04-14 21:10:10 +0400

    Пагинация

--- app/Http/Controllers/API/Product/IndexController.php
+++ app/Http/Controllers/API/Product/IndexController.php
@@ -16,7 +16,7 @@ class IndexController extends Controller
     {
         $data = $request->validated();
         $filter = app()->make(ProductFilter::class, ['queryParams' => array_filter($data)]);
-        $products = Product::filter($filter)->get();
+        $products = Product::filter($filter)->paginate(9, ['*'], 'page', $data['page']);
         return IndexProductResource::collection($products);
     }
 }
--- app/Http/Requests/API/Product/IndexRequest.php
+++ app/Http/Requests/API/Product/IndexRequest.php
@@ -28,6 +28,7 @@ class IndexRequest extends FormRequest
             'tags' => 'nullable|array',
             'colors' => 'nullable|array',
             'prices' => 'nullable|array',
+            'page' => 'required|integer',
         ];
     }
 }
